474f8c385474a412cf0b50d76673802b224afc16
Daenakis <zloypelmen@bk.ru>
2018-07-13 17:52:49 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+var budgetController = (function() {
+
+    
+    
+})();
+
+var UIController = (function() {
+    
+    
+    
+})();
+
+
+var controller = (function(budgetCont, UICont) {
+    
+    document.querySelector('.add__btn').addEventListener('click', function() {
+        console.log('Button was'); 
+    });
+    
+})(budgetController, UIController); 
